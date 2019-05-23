f3fcc253703e51caf960653c2aa2cc4457569cdf
Zandraa Attila Perjesi <zandraa.perjesi.ext@evosoft.com>
2019-05-23 15:27:18 +0200

    Add mega and deca to the mapping

--- src/main/java/UnitConverter.java
+++ src/main/java/UnitConverter.java
@@ -1,24 +1,24 @@
 class UnitConverter {
 
     double convertFromTo(double value, String changeFrom, String changeTo) {
-        double fromProp = changeFrom.length() == 2 ? metric(changeFrom.charAt(0)) : 1;
-        double toProp = changeTo.length() == 2 ? metric(changeTo.charAt(0)) : 1;
+        double fromProp = changeFrom.length() > 1 ? metric(changeFrom.substring(0, changeFrom.length() - 1)) : 1;
+        double toProp = changeTo.length() > 1 ? metric(changeTo.substring(0,changeTo.length() - 1)) : 1;
         return value * fromProp / toProp;
     }
 
-    private double metric(char metricString) {
+    private double metric(String metricString) {
         switch(metricString) {
-            case 't': return 1000000000000L;
-            case 'g': return 1000000000;
-//            case "mega": return 1; colliding with `milli`
-            case 'k': return 1000;
-            case 'h': return 100;
-//            case "deca": return 1; colliding with `deci`
-            case 'd': return 0.1;
-            case 'c': return 0.01;
-            case 'm': return 0.001;
-            case 'µ': return 0.000001;
-            case 'n': return 0.000000001;
+            case "t": return 1000000000000L;
+            case "g": return 1000000000;
+            case "M": return 1000000;
+            case "k": return 1000;
+            case "h": return 100;
+            case "da": return 10;
+            case "d": return 0.1;
+            case "c": return 0.01;
+            case "m": return 0.001;
+            case "µ": return 0.000001;
+            case "n": return 0.000000001;
             default: return -1;
         }
     }
